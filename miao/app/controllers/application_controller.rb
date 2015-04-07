2ac52b5b0e0be2cc6099943d4731bda24245e2de
flowerwrong <sysuyangkang@gmail.com>
2015-04-07 17:19:40 +0800

    Format app controller and helper and del none use method.

--- miao/app/controllers/application_controller.rb
+++ miao/app/controllers/application_controller.rb
@@ -10,59 +10,59 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_request
 
 	rescue_from NotAuthenticatedError do
-		render json: { error: 'Not Authorized' }, status: :unauthorized
+	  render json: { error: 'Not Authorized' }, status: :unauthorized
 	end
 
 	rescue_from AuthenticationTimeoutError do
-		render json: { error: 'Auth token is expired' }, status: 419
+	  render json: { error: 'Auth token is expired' }, status: 419
 	end
 
 	rescue_from NoAuthTokenError do
-		render json: { error: 'Auth token is not sent' }, status: 418
+	  render json: { error: 'Auth token is not sent' }, status: 418
 	end
 
 	rescue_from ActiveRecord::RecordNotFound do
-		render json: { error: 'Record not found' }, status: :not_found
+	  render json: { error: 'Record not found' }, status: :not_found
 	end
 
 	layout false
 
 	def method_missing
-		render json: { error: '404' }, status: :not_found
+	  render json: { error: '404' }, status: :not_found
 	end
 
 	protected
 
 	# In Rails 4.2 and above for angular X-XSRF-TOKEN
 	def verified_request?
-		super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+	  super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
 	end
 
 	private
 
 	# Based on the user_id inside the token payload, find the user.
 	def set_current_user
-		@auth_token = http_auth_content
-		unless @auth_token.blank?
-			@current_user ||= User.where(auth_token: @auth_token).first
-		end
+	  @auth_token = http_auth_content
+	  unless @auth_token.blank?
+	    @current_user ||= User.where(auth_token: @auth_token).first
+	  end
 	end
 
 	def authenticate_request
-		if !@current_user
-			fail NotAuthenticatedError
-		elsif auth_token_expired?
-			fail AuthenticationTimeoutError
-		end
+	  if !@current_user
+	    fail NotAuthenticatedError
+	  elsif auth_token_expired?
+		fail AuthenticationTimeoutError
+	  end
 	end
 
 	def auth_token_expired?
-		unless @current_user.blank?
-			unless @current_user.expiration_time.blank?
-				@current_user.expiration_time.to_i <= Time.now.to_i
-			end
-		else
-			fail NotAuthenticatedError
+	  unless @current_user.blank?
+		unless @current_user.expiration_time.blank?
+		  @current_user.expiration_time.to_i <= Time.now.to_i
 		end
+	  else
+		fail NotAuthenticatedError
+	  end
 	end
 end
--- miao/app/helpers/application_helper.rb
+++ miao/app/helpers/application_helper.rb
@@ -1,26 +1,10 @@
 # All app helper
 #
 module ApplicationHelper
-  # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  # Returns the current logged-in user (if any).
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
-  end
-
-  # Logs out the current user.
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
   end
 
   def require_login
@@ -43,10 +27,6 @@ module ApplicationHelper
         nil
       end
     end
-  end
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 
   def set_locale
