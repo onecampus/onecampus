e204ce1e7cf15be707997b3e89a3166ed840f762
flowerwrong <sysuyangkang@gmail.com>
2015-04-10 17:27:57 +0800

    Add chinacity model test.

--- miao/test/models/china_city_test.rb
+++ miao/test/models/china_city_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ChinaCityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  self.use_instantiated_fixtures = true
+
+  test 'should not save chinacity with level more than 3' do
+    assert_not @level_more_3.save
+  end
+
+  test 'should not save chinacity with area code less 100000' do
+    assert_not @area_code_less_100000.save
+  end
+
+  test 'should not save chinacity without area' do
+    assert_not @without_area.save
+  end
 end
